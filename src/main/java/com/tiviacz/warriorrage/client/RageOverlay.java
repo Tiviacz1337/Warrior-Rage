ab4605bd3c6ccaf5ef856ddcf563f728a72de72d
Kamil <34790800+Tiviacz1337@users.noreply.github.com>
2024-05-07 12:22:08 +0200

    ✨ 1.20.1 fabric

--- src/main/java/com/tiviacz/warriorrage/client/RageOverlay.java
+++ src/main/java/com/tiviacz/warriorrage/client/RageOverlay.java
@@ -1,16 +1,14 @@
 package com.tiviacz.warriorrage.client;
 
-import com.mojang.blaze3d.systems.RenderSystem;
 import com.tiviacz.warriorrage.WarriorRage;
 import com.tiviacz.warriorrage.component.ComponentUtils;
 import com.tiviacz.warriorrage.component.IRage;
 import com.tiviacz.warriorrage.component.Rage;
 import com.tiviacz.warriorrage.config.WarriorRageConfig;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.util.Window;
-import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
@@ -28,7 +26,7 @@ public class RageOverlay extends Screen
         this.mainWindow = MinecraftClient.getInstance().getWindow();
     }
 
-    public void renderOverlay(MatrixStack matrices)
+    public void renderOverlay(DrawContext drawContext)
     {
         PlayerEntity player = mc.player;
 
@@ -46,27 +44,22 @@ public class RageOverlay extends Screen
                 float durationProgress = (float)rage.getRemainingRageDuration() / Rage.DEFAULT_RAGE_DURATION;
                 int k = (int)(durationProgress * (183.0F));
 
-                RenderSystem.setShader(GameRenderer::getPositionTexShader);
-                RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
-                RenderSystem.setShaderTexture(0, texture);
-
                 if(WarriorRageConfig.getConfig().renderRageBar)
                 {
-                    drawTexture(matrices, screenWidth / 2 - 91, screenHeight - 32 + 3, 0, 69, k, 5);
+                    drawContext.drawTexture(texture, screenWidth / 2 - 91, screenHeight - 32 + 3, 0, 69, k, 5);
                 }
 
                 if(WarriorRageConfig.getConfig().renderRageIcon)
                 {
-                    drawTexture(matrices, screenWidth / 2 + 94 + WarriorRageConfig.getConfig().offsetX, screenHeight - 32 + 16 + WarriorRageConfig.getConfig().offsetY, 0, 0, 14, 14);
-
+                    drawContext.drawTexture(texture, screenWidth / 2 + 94 + WarriorRageConfig.getConfig().offsetX, screenHeight - 32 + 16 + WarriorRageConfig.getConfig().offsetY, 0, 0, 14, 14);
                     String s = "" + rage.getCurrentKillCount();
                     int i1 = (screenWidth - mc.textRenderer.getWidth(s)) / 2 + 115 + WarriorRageConfig.getConfig().offsetX;
                     int j1 = screenHeight - 31 + 18 + WarriorRageConfig.getConfig().offsetY;
-                    mc.textRenderer.draw(matrices, s, (float)(i1 + 1), (float)j1, 0);
-                    mc.textRenderer.draw(matrices, s, (float)(i1 - 1), (float)j1, 0);
-                    mc.textRenderer.draw(matrices, s, (float)i1, (float)(j1 + 1), 0);
-                    mc.textRenderer.draw(matrices, s, (float)i1, (float)(j1 - 1), 0);
-                    mc.textRenderer.draw(matrices, s, (float)i1, (float)j1, 6362132);
+                    drawContext.drawText(mc.textRenderer, s, (i1 + 1), j1, 0, false);
+                    drawContext.drawText(mc.textRenderer, s, (i1 - 1), j1, 0, false);
+                    drawContext.drawText(mc.textRenderer, s, i1, (j1 + 1), 0, false);
+                    drawContext.drawText(mc.textRenderer, s, i1, (j1 - 1), 0, false);
+                    drawContext.drawText(mc.textRenderer, s, i1, j1, 6362132, false);
                 }
             }
         }
--- src/main/java/com/tiviacz/warriorrage/mixin/InGameHudMixin.java
+++ src/main/java/com/tiviacz/warriorrage/mixin/InGameHudMixin.java
@@ -4,8 +4,8 @@ import com.tiviacz.warriorrage.client.RageOverlay;
 import com.tiviacz.warriorrage.component.ComponentUtils;
 import com.tiviacz.warriorrage.config.WarriorRageConfig;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
-import net.minecraft.client.util.math.MatrixStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -15,14 +15,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class InGameHudMixin
 {
     @Inject(at = @At(value = "TAIL"), method = "renderExperienceBar")
-    private void renderOverlay(MatrixStack matrices, int x, CallbackInfo ci)
+    private void renderOverlay(DrawContext drawContext, int x, CallbackInfo ci)
     {
         if((WarriorRageConfig.getConfig().renderRageBar || WarriorRageConfig.getConfig().renderRageIcon) && !MinecraftClient.getInstance().options.hudHidden)
         {
             if(ComponentUtils.RAGE.isProvidedBy(MinecraftClient.getInstance().player))
             {
                 RageOverlay gui = new RageOverlay();
-                gui.renderOverlay(matrices);
+                gui.renderOverlay(drawContext);
             }
         }
     }
